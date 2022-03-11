19aedb7686b8c047e54acfb973d3c7de1dde35a6
coinlordd <35095979+coinlordd@users.noreply.github.com>
2022-03-11 17:33:53 +0100

    Route issue attempt #1

--- next.config.js
+++ next.config.js
@@ -2,22 +2,6 @@ module.exports = {
   images: {
     domains: ['raw.githubusercontent.com', 'assets.spookyswap.finance'],
   },
-  exportPathMap: async function () {
-    return {
-      '/convert': {
-        page: '/convert',
-      },
-      '/vote': {
-        page: '/vote',
-      },
-      '/borrow': {
-        page: '/borrow',
-      },
-      '/borrow/:contract': {
-        page: '/borrow/[contract].tsx',
-      },
-    }
-  },
   async redirects() {
     return [
       {

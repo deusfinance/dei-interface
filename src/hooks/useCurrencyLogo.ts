75db0c6be6c7504476b66f1ff4e319dca092fc03
coinlordd <35095979+coinlordd@users.noreply.github.com>
2022-03-12 11:45:05 +0100

    Prepare production release

--- src/hooks/useCurrencyLogo.ts
+++ src/hooks/useCurrencyLogo.ts
@@ -8,9 +8,12 @@ import DEI_LOGO from '/public/static/images/tokens/dei.svg'
 const LogoMap: { [contractOrSymbol: string]: string } = {
   FTM: 'https://assets.spookyswap.finance/tokens/FTM.png',
   '0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83': 'https://assets.spookyswap.finance/tokens/wFTM.png', // wFTM
-  '0x82f0B8B456c1A451378467398982d4834b6829c1': 'https://assets.spookyswap.finance/tokens/MIM.png', // MIM
-  '0xCD9a7E6383F218e13c868c2328E9F4ad8a65f312': 'https://assets.spookyswap.finance/tokens/USDC.png', // TEMPORARY
-  '0x4A4573B03B0800e24dEcD8fE497BFeD98ee344B8': DEI_LOGO, // TEMPORARY
+  '0x04068DA6C83AFCFA0e13ba15A6696662335D5B75': 'https://assets.spookyswap.finance/tokens/USDC.png', // USDC
+  '0xDE12c7959E1a72bbe8a5f7A1dc8f8EeF9Ab011B3': DEI_LOGO, // DEI
+
+  // THESE ASSETS ARE TEMPORARILY / TESTNET TOKENS
+  '0xCD9a7E6383F218e13c868c2328E9F4ad8a65f312': 'https://assets.spookyswap.finance/tokens/USDC.png',
+  '0x4A4573B03B0800e24dEcD8fE497BFeD98ee344B8': DEI_LOGO,
 }
 
 export default function useCurrencyLogo(contract?: string): string {

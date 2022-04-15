5f5ec5c93bf54c15ef771c6493d1225d2487210e
ToTheMoon.FTM <tothemoon.ftm@gmail.com>
2022-04-16 00:09:08 +0800

    Fix underlying network changed error

--- src/utils/library.ts
+++ src/utils/library.ts
@@ -2,14 +2,7 @@ import { Web3Provider } from '@ethersproject/providers'
 import { NETWORK_POLLING_INTERVALS } from 'constants/misc'
 
 export function getLibrary(provider: any): Web3Provider {
-  const library = new Web3Provider(
-    provider,
-    typeof provider.chainId === 'number'
-      ? provider.chainId
-      : typeof provider.chainId === 'string'
-      ? parseInt(provider.chainId)
-      : 'any'
-  )
+  const library = new Web3Provider(provider, 'any')
   library.pollingInterval = 15000
   library.detectNetwork().then((network) => {
     const networkPollingInterval = NETWORK_POLLING_INTERVALS[network.chainId]

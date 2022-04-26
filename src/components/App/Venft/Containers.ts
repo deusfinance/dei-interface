44c6b0b9a6ebae6f7d94d98c1d53503b32e4dbd3
Ali Mahdiyar <alimahdiyar77@gmail.com>
2022-04-26 19:56:13 +0430

    feat: buy venft filter sliders

--- src/components/App/Venft/Containers.ts
+++ src/components/App/Venft/Containers.ts
@@ -12,22 +12,6 @@ export const Wrapper = styled(Container)`
   margin-top: 50px;
   width: clamp(250px, 90%, 1200px);
 
-  & > * {
-    &:nth-child(1) {
-      margin-bottom: 25px;
-      display: flex;
-      flex-flow: row nowrap;
-      width: 100%;
-      gap: 15px;
-
-      & > * {
-        &:last-child {
-          max-width: 300px;
-        }
-      }
-    }
-  }
-
   ${({ theme }) => theme.mediaWidth.upToMedium`
     margin-top: 20px;
   `}
--- src/pages/venft/buy.tsx
+++ src/pages/venft/buy.tsx
@@ -1,12 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Hero, { HeroSubtext } from 'components/Hero'
 import Disclaimer from 'components/Disclaimer'
 import { SearchField, useSearch } from 'components/App/Borrow'
-import { Container, TableBuy, Wrapper } from 'components/App/Venft'
+import { Container, Slider, TableBuy, Wrapper } from 'components/App/Venft'
+import styled from 'styled-components'
+
+const FiltersWrapper = styled.div`
+  display: flex;
+  margin-bottom: 25px;
+`
+
+const SearchFieldWrapper = styled.div`
+  width: 34%;
+`
+
+const RangeWrapper = styled.div`
+  margin-left: 10px;
+  width: 33%;
+  vertical-align: center;
+  display: flex;
+  align-items: end;
+`
+
+const RangeLabelWrapper = styled.span`
+  vertical-align: center;
+  font-size: 11px;
+  width: 30px;
+  padding-bottom: 5px;
+  text-align: center;
+`
+
+const SliderWrapper = styled.span`
+  text-align: center;
+  width: 100%;
+  padding: 10px;
+`
 
 export default function Buy() {
   const { searchProps } = useSearch()
+  const [amountRange, setAmountRange] = useState([0, 100])
+  const [minAmount, maxAmount] = amountRange
+
+  const [timeRange, setTimeRange] = useState([0, 100])
+  const [minTime, maxTime] = timeRange
 
   return (
     <Container>
@@ -15,9 +52,25 @@ export default function Buy() {
         <HeroSubtext>Buy veNFT</HeroSubtext>
       </Hero>
       <Wrapper>
-        <div>
-          <SearchField searchProps={searchProps} />
-        </div>
+        <FiltersWrapper>
+          <SearchFieldWrapper>
+            <SearchField searchProps={searchProps} />
+          </SearchFieldWrapper>
+          <RangeWrapper>
+            <RangeLabelWrapper>{minAmount}</RangeLabelWrapper>
+            <SliderWrapper>
+              <Slider label={'amount'} min={0} max={100} values={amountRange} setValues={setAmountRange} />
+            </SliderWrapper>
+            <RangeLabelWrapper>{maxAmount}</RangeLabelWrapper>
+          </RangeWrapper>
+          <RangeWrapper>
+            <RangeLabelWrapper>{minTime}</RangeLabelWrapper>
+            <SliderWrapper>
+              <Slider label={'time'} min={0} max={100} values={timeRange} setValues={setTimeRange} />
+            </SliderWrapper>
+            <RangeLabelWrapper>{maxTime}</RangeLabelWrapper>
+          </RangeWrapper>
+        </FiltersWrapper>
         <TableBuy />
       </Wrapper>
       <Disclaimer />

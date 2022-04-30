dd1bb408a3d105ad3ea26e05f9de9d422fd3d5d3
Ali Mahdiyar <alimahdiyar77@gmail.com>
2022-04-30 15:25:30 +0430

    refactor: refactor e2e tests

--- cypress/integration/venftsell.test.ts
+++ cypress/integration/venftsell.test.ts
@@ -1,9 +1,7 @@
 import {
-  CustomizedBridge,
   HasVenftToSellBridge,
   provider,
   signer,
-  TEST_ADDRESS_NEVER_USE,
   TEST_ADDRESS_NEVER_USE_SHORTENED,
   ZeroBalanceVenftBridge,
 } from '../support/commands'
@@ -12,29 +10,29 @@ describe('Landing Page', () => {
   const setupEthBridge = () => {
     cy.on('window:before:load', (win) => {
       // @ts-ignore
-      win.ethereum = new CustomizedBridge(signer, provider)
+      win.ethereum = new ZeroBalanceVenftBridge(signer, provider)
     })
   }
 
-  it.skip('is connected', () => {
+  it('is connected', () => {
     setupEthBridge()
     cy.visit('/venft/sell/')
     cy.get('[data-testid=wallet-connect]', { timeout: 1000 }).contains(TEST_ADDRESS_NEVER_USE_SHORTENED)
   })
 
-  it.skip('gets VeNFT balance', () => {
+  it('gets VeNFT balance', () => {
     const ethBridge = new ZeroBalanceVenftBridge(signer, provider)
     cy.on('window:before:load', (win) => {
       // @ts-ignore
       win.ethereum = ethBridge
-      cy.spy(ethBridge, 'VeNFTBalanceOfSpy')
+      cy.spy(ethBridge, 'VeNFTBalanceOf')
     })
 
     cy.visit('/venft/sell/')
 
     cy.wait(1000)
     cy.window().then((win) => {
-      expect(ethBridge.VeNFTBalanceOfSpy).to.have.calledWith(TEST_ADDRESS_NEVER_USE)
+      expect(ethBridge.VeNFTBalanceOf).to.have.callCount(1)
     })
     cy.get(`[data-testid=venft-sell-no-results]`)
   })
@@ -44,7 +42,7 @@ describe('Landing Page', () => {
     cy.on('window:before:load', (win) => {
       // @ts-ignore
       win.ethereum = ethBridge
-      cy.spy(ethBridge, 'VeNFTBalanceOfSpy')
+      cy.spy(ethBridge, 'VeNFTBalanceOf')
       cy.spy(ethBridge, 'tokenOfOwnerByIndex')
       cy.spy(ethBridge, 'veNFTLockedData')
     })
@@ -53,7 +51,7 @@ describe('Landing Page', () => {
     const expectTokenId = ethBridge.tokens.sort((a: any, b: any) => a.tokenId - b.tokenId)[1].tokenId
     cy.get(`[data-testid=venft-sell-row-1-token-id]`, { timeout: 2000 }).contains(expectTokenId)
     cy.window().then((win) => {
-      expect(ethBridge.VeNFTBalanceOfSpy).to.have.calledWith(TEST_ADDRESS_NEVER_USE)
+      expect(ethBridge.VeNFTBalanceOf).to.have.callCount(1)
       expect(ethBridge.tokenOfOwnerByIndex).to.have.callCount(2)
       expect(ethBridge.veNFTLockedData).to.have.callCount(2)
     })

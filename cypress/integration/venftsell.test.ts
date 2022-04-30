dab393c0df1ba9f9d467c2562f1016a1a181b055
Ali Mahdiyar <alimahdiyar77@gmail.com>
2022-04-30 12:54:25 +0430

    feat: add venft list e2e test

--- cypress/integration/venftsell.test.ts
+++ cypress/integration/venftsell.test.ts
@@ -1,5 +1,6 @@
 import {
   CustomizedBridge,
+  HasVenftToSellBridge,
   provider,
   signer,
   TEST_ADDRESS_NEVER_USE,
@@ -15,13 +16,13 @@ describe('Landing Page', () => {
     })
   }
 
-  it('is connected', () => {
+  it.skip('is connected', () => {
     setupEthBridge()
     cy.visit('/venft/sell/')
-    cy.get('[data-cy=wallet-connect]', { timeout: 1000 }).contains(TEST_ADDRESS_NEVER_USE_SHORTENED)
+    cy.get('[data-testid=wallet-connect]', { timeout: 1000 }).contains(TEST_ADDRESS_NEVER_USE_SHORTENED)
   })
 
-  it('loads VeNFT list', () => {
+  it.skip('gets VeNFT balance', () => {
     const ethBridge = new ZeroBalanceVenftBridge(signer, provider)
     cy.on('window:before:load', (win) => {
       // @ts-ignore
@@ -36,5 +37,25 @@ describe('Landing Page', () => {
       expect(ethBridge.VeNFTBalanceOfSpy).to.have.calledWith(TEST_ADDRESS_NEVER_USE)
     })
     cy.get(`[data-testid=venft-sell-no-results]`)
+  })
+
+  it('loads VeNFT list', () => {
+    const ethBridge = new HasVenftToSellBridge(signer, provider)
+    cy.on('window:before:load', (win) => {
+      // @ts-ignore
+      win.ethereum = ethBridge
+      cy.spy(ethBridge, 'VeNFTBalanceOfSpy')
+      cy.spy(ethBridge, 'tokenOfOwnerByIndex')
+      cy.spy(ethBridge, 'veNFTLockedData')
+    })
+
+    cy.visit('/venft/sell/')
+    const expectTokenId = ethBridge.tokens.sort((a: any, b: any) => a.tokenId - b.tokenId)[1].tokenId
+    cy.get(`[data-testid=venft-sell-row-1-token-id]`, { timeout: 2000 }).contains(expectTokenId)
+    cy.window().then((win) => {
+      expect(ethBridge.VeNFTBalanceOfSpy).to.have.calledWith(TEST_ADDRESS_NEVER_USE)
+      expect(ethBridge.tokenOfOwnerByIndex).to.have.callCount(2)
+      expect(ethBridge.veNFTLockedData).to.have.callCount(2)
+    })
   })
 })
--- cypress/support/commands.d.ts
+++ cypress/support/commands.d.ts
@@ -2,6 +2,7 @@ export const TEST_ADDRESS_NEVER_USE: string
 
 export const CustomizedBridge: any
 export const ZeroBalanceVenftBridge: any
+export const HasVenftToSellBridge: any
 
 export const provider: any
 export const signer: any

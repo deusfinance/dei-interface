8f246d9e210a2b9135cd797c3f0a25056e5f7331
Ali Mahdiyar <alimahdiyar77@gmail.com>
2022-05-04 14:17:30 +0430

    refactor: venft sell e2e test

--- cypress/integration/03-venftsell.test.ts
+++ cypress/integration/03-venftsell.test.ts
@@ -2,6 +2,12 @@ import { provider, SellVeNFTBridge, signer } from '../support/commands'
 import { tokenListSorted } from '../utils/data'
 
 describe('VeNFT Sell', () => {
+  const sellVenft = (ethBridge: any, tokenId: number) => {
+    cy.get(`[data-testid=venft-sell-row-1-action]`).contains('Sell')
+    cy.get(`[data-testid=venft-sell-row-1-action]`).click()
+    cy.wait(500)
+    cy.get('[data-testid=explorer-link-success-box]')
+  }
   it('sells VeNFT', () => {
     const ethBridge = new SellVeNFTBridge(signer, provider)
     cy.on('window:before:load', (win) => {
@@ -12,12 +18,9 @@ describe('VeNFT Sell', () => {
 
     cy.visit('/venft/sell/')
     const expectTokenId = tokenListSorted[1].tokenId
-    cy.get(`[data-testid=venft-sell-row-1-action]`).contains('Sell')
-    cy.get(`[data-testid=venft-sell-row-1-action]`).click()
-    cy.wait(500)
+    sellVenft(ethBridge, expectTokenId)
     cy.window().then((win) => {
       expect(ethBridge.sellVeNFTSpy).to.have.calledWith(expectTokenId)
     })
-    cy.get('[data-testid=explorer-link-success-box]')
   })
 })

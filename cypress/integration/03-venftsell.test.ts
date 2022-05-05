bdbc9fd16388cfbb684913e8a70672f570b04f59
Ali Mahdiyar <alimahdiyar77@gmail.com>
2022-05-05 17:27:28 +0430

    feat: withdraw after venft sell

--- cypress/integration/03-venftsell.test.ts
+++ cypress/integration/03-venftsell.test.ts
@@ -1,9 +1,17 @@
-import { provider, SellVeNFTBridge, signer, WithdrawVeNFTBridge } from '../support/commands'
+import { provider, SellVeNFTBridge, signer } from '../support/commands'
 import { tokenListSorted } from '../utils/data'
 import { fromWei } from '../../src/utils/numbers'
 
 describe('VeNFT Sell', () => {
-  it('sells VeNFT', () => {
+  function sellVeNFT(tokenIndex: number) {
+    cy.wait(500)
+    cy.get(`[data-testid=venft-fsolid-withdraw]`).should('not.exist')
+    cy.get(`[data-testid=venft-sell-row-${tokenIndex}-action]`).contains('Sell')
+    cy.get(`[data-testid=venft-sell-row-${tokenIndex}-action]`).click()
+    cy.get('[data-testid=explorer-link-success-box]')
+  }
+
+  it('sells veNFT', () => {
     const tokenIndex = 1
     const ethBridge = new SellVeNFTBridge(signer, provider)
     cy.on('window:before:load', (win) => {
@@ -13,19 +21,17 @@ describe('VeNFT Sell', () => {
     })
 
     cy.visit('/venft/sell/')
-    cy.wait(500)
-    cy.get(`[data-testid=venft-fsolid-withdraw]`).should('not.exist')
-    cy.get(`[data-testid=venft-sell-row-${tokenIndex}-action]`).contains('Sell')
-    cy.get(`[data-testid=venft-sell-row-${tokenIndex}-action]`).click()
-    cy.get('[data-testid=explorer-link-success-box]')
+    sellVeNFT(tokenIndex)
     cy.window().then((win) => {
       const expectTokenId = tokenListSorted[tokenIndex].tokenId
       expect(ethBridge.sellVeNFTSpy).to.have.calledWith(expectTokenId)
     })
   })
 
-  it('withdraws fsolid after selling VeNFT', () => {
-    const ethBridge = new WithdrawVeNFTBridge(signer, provider)
+  it('withdraws fsolid after selling veNFT', () => {
+    const expectTokenId = tokenListSorted[1].tokenId
+    const ethBridge = new SellVeNFTBridge(signer, provider)
+    ethBridge.setWithdrawFsolidTokenId(expectTokenId)
     cy.on('window:before:load', (win) => {
       // @ts-ignore
       win.ethereum = ethBridge
@@ -33,8 +39,43 @@ describe('VeNFT Sell', () => {
     })
     cy.visit('/venft/sell/')
     cy.get(`[data-testid=venft-fsolid-withdraw]`).should('exist')
-    const expectTokenId = tokenListSorted[1].tokenId
     const withdrawAmount = parseFloat(fromWei(tokenListSorted[1].needsAmount))
+    cy.get(`[data-testid=venft-fsolid-withdraw-token-id]`).contains(expectTokenId)
+    cy.get(`[data-testid=venft-fsolid-withdraw-amount]`).contains(parseFloat(fromWei(withdrawAmount)))
+    cy.window().then((win) => {
+      ethBridge.setWithdrawFsolidTokenId(0)
+    })
+    cy.get(`[data-testid=venft-fsolid-withdraw-action]`).click()
+    cy.get('[data-testid=explorer-link-success-box]')
+    cy.window().then((win) => {
+      expect(ethBridge.withdrawFSolid).to.have.callCount(1)
+    })
+    cy.get(`[data-testid=venft-fsolid-withdraw]`).should('not.exist')
+  })
+
+  it('sells veNFT and withdraws fsolid', () => {
+    const tokenIndex = 1
+    const expectTokenId = tokenListSorted[tokenIndex].tokenId
+
+    const ethBridge = new SellVeNFTBridge(signer, provider)
+    cy.on('window:before:load', (win) => {
+      // @ts-ignore
+      win.ethereum = ethBridge
+      cy.spy(ethBridge, 'withdrawFSolid')
+      cy.spy(ethBridge, 'sellVeNFTSpy')
+    })
+    cy.visit('/venft/sell/')
+    cy.wait(500)
+    cy.get(`[data-testid=venft-fsolid-withdraw]`).should('not.exist')
+    cy.get(`[data-testid=venft-sell-row-${tokenIndex}-action]`).contains('Sell')
+    cy.get(`[data-testid=venft-sell-row-${tokenIndex}-action]`).click()
+    cy.window().then((win) => {
+      expect(ethBridge.sellVeNFTSpy).to.have.calledWith(expectTokenId)
+      ethBridge.setWithdrawFsolidTokenId(expectTokenId)
+    })
+    cy.get('[data-testid=explorer-link-success-box]')
+    cy.get(`[data-testid=venft-fsolid-withdraw]`).should('exist')
+    const withdrawAmount = parseFloat(fromWei(tokenListSorted[tokenIndex].needsAmount))
     cy.get(`[data-testid=venft-fsolid-withdraw-token-id]`).contains(expectTokenId)
     cy.get(`[data-testid=venft-fsolid-withdraw-amount]`).contains(parseFloat(fromWei(withdrawAmount)))
     cy.get(`[data-testid=venft-fsolid-withdraw-action]`).click()

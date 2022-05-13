d4de1350d1c9baad67ba9963d741e6d722b3c75f
Ali Mahdiyar <alimahdiyar77@gmail.com>
2022-05-13 12:22:50 +0430

    refactor: buy venft e2e tests

--- cypress/integration/04-venftbuy.test.ts
+++ cypress/integration/04-venftbuy.test.ts
@@ -1,22 +1,30 @@
-import { BuyVeNFTBridge, provider, signer } from '../support/commands'
+import { getCustomizedBridge } from '../support/commands'
 import { tokenListSorted } from '../utils/data'
-import VAULT_ABI from '../../src/constants/abi/Vault.json'
 // @ts-ignore
-import { BaseVaultHandler } from '../utils/abihandlers/Vault'
+import { vaultHandler } from '../utils/abihandlers/Vault'
 import { SupportedChainId } from '../../src/constants/chains'
-import { Vault } from '../../src/constants/addresses'
+import { Multicall2, Vault } from '../../src/constants/addresses'
+import { multicallHandler } from '../utils/abihandlers/Multicall'
+
+describe('render VeNFT Buy Page', () => {
+  it('renders page when wallet is not connected', () => {
+    cy.visit('/venft/buy/')
+    cy.get('[data-testid=venft-buy-page]')
+  })
+})
 
 describe('VeNFT Buy', () => {
   const tokenIndex = 1
   const expectTokenId = tokenListSorted[tokenIndex].tokenId
-  const vaultHandler = {
-    abi: VAULT_ABI,
-    handler: new BaseVaultHandler(),
-  }
+  const ethBridge = getCustomizedBridge()
 
-  it('renders page when wallet is not connected', () => {
-    cy.visit('/venft/buy/')
-    cy.get('[data-testid=venft-buy-page]')
+  beforeEach(() => {
+    cy.on('window:before:load', (win) => {
+      // @ts-ignore
+      win.ethereum = ethBridge
+    })
+    ethBridge.setHandler(Multicall2[SupportedChainId.FANTOM], multicallHandler)
+    ethBridge.setHandler(Vault[SupportedChainId.FANTOM], vaultHandler)
   })
 
   function buyVeNFT() {
@@ -28,18 +36,11 @@ describe('VeNFT Buy', () => {
   }
 
   it('buys veNFT', () => {
-    const ethBridge = new BuyVeNFTBridge(signer, provider)
-    ethBridge.setHandler(Vault[SupportedChainId.FANTOM], vaultHandler)
-    cy.on('window:before:load', (win) => {
-      // @ts-ignore
-      win.ethereum = ethBridge
-      cy.spy(vaultHandler.handler, 'buyVeNFTSpy')
-    })
+    cy.spy(vaultHandler.handler, 'buyVeNFTSpy')
 
     cy.visit('/venft/buy/')
     buyVeNFT()
     cy.window().then((win) => {
-      const expectTokenId = tokenListSorted[tokenIndex].tokenId
       expect(vaultHandler.handler.buyVeNFTSpy).to.have.calledWith(expectTokenId)
     })
   })

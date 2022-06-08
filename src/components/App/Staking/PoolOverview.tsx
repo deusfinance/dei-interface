d0fd1f9cfa831517e2cfdfb12aea2ad1ff2ee5fb
ehsan1814 <mr.ehsan.kaji@gmail.com>
2022-06-08 19:35:59 +0430

    feat: add stakingOverview component

--- src/components/App/Staking/PoolOverview.tsx
+++ src/components/App/Staking/PoolOverview.tsx
@@ -6,33 +6,39 @@ import { PrimaryButton } from 'components/Button'
 import ImageWithFallback from 'components/ImageWithFallback'
 import useCurrencyLogo from 'hooks/useCurrencyLogo'
 import { Loader } from 'components/Icons'
+// import { formatDollarAmount } from 'utils/numbers'
+import { BDEI_ADDRESS } from '../../../constants/addresses'
+import { SupportedChainId } from 'constants/chains'
 
-const Container = styled.div`
-  /* display: flex; */
-  /* flex-flow: column nowrap; */
-  /* overflow: visible; */
-  /* margin: 0 auto; */
-`
-
-const Wrapper = styled(Container)`
-  margin: 0 auto;
-  /* width: clamp(250px, 90%, 500px); */
+const Wrapper = styled.div`
   background-color: rgb(13 13 13);
-  padding: 20px 15px;
-  /* border: 1px solid rgb(0, 0, 0); */
-  /* border-radius: 15px; */
-  /* justify-content: center; */
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
-  gap: 0 95px;
+  flex-wrap: wrap;
 
   & > * {
-    /* margin: 0 25px; */
     text-align: center;
-    /* line-height: 25px; */
+    /* ${({ theme }) => theme.mediaWidth.upToMedium`
+      margin-top: 30px;
+  `} */
   }
+  ${({ theme }) => theme.mediaWidth.upToSmall`
+    & > * {
+    &:nth-child(1) {
+      width: 100%;
+      max-width: 100%;
+    }
+
+    &:nth-child(4) {
+      width: 100%;
+      margin-top: 20px;
+      max-width: 100%;
+      flex-direction: row;
+    }
+  }
+  `}
 `
 
 const Name = styled.div`
@@ -58,25 +64,42 @@ const PoolInfo = styled.div`
   max-width: 25%;
 `
 
-const DepositButton = styled(PrimaryButton)`
-  margin-bottom: 10px;
-  border-radius: 15px;
+const InfoTitle = styled.div`
+  color: rgb(101, 101, 101);
+  font-size: 16px;
 `
 
 const WithdrawButton = styled(PrimaryButton)`
-  border-radius: 15px;
+  border-radius: 4px;
+  width: 160px;
+  ${({ theme }) => theme.mediaWidth.upToSmall`
+    height: 40px;
+  `}
+`
+
+const DepositButton = styled(WithdrawButton)`
+  margin-bottom: 16px;
+  ${({ theme }) => theme.mediaWidth.upToSmall`
+    margin-bottom: 0;
+    margin-right: 16px;
+  `}
 `
 
 const ItemValue = styled.div`
-  color: ${({ theme }) => theme.yellow3};
+  color: white;
   margin-left: 5px;
+  font-size: 20px;
+  font-weight: 400;
+`
+
+const SymbolText = styled.div`
+  margin-left: 10px;
 `
 
 export default function PoolOverview() {
-  const showLoader = true
+  const showLoader = false
 
   return (
-    // <Container>
     <Wrapper>
       <Name> DEI-BDEI </Name>
       <Coins>
@@ -84,31 +107,28 @@ export default function PoolOverview() {
           <BalanceRow address={'DEI'} symbol="DEI" />
         </Row>
         <Row>
-          <BalanceRow address={'DEI'} symbol="BDEI" />
+          <BalanceRow address={BDEI_ADDRESS[SupportedChainId.FANTOM]} symbol="bDEI" />
         </Row>
       </Coins>
+      {/* <PoolInfo>
+        <InfoTitle>TVL</InfoTitle>
+        <ItemValue>{showLoader ? <Loader /> : formatDollarAmount(5124305)}</ItemValue>
+      </PoolInfo> */}
+      {/* <PoolInfo>
+        <InfoTitle>24h Volume</InfoTitle>
+        <ItemValue>{showLoader ? <Loader /> : formatDollarAmount(1646501)}</ItemValue>
+      </PoolInfo> */}
       <PoolInfo>
-        <Row>
-          TVL <ItemValue>{showLoader ? <Loader /> : '12'}</ItemValue>
-        </Row>
-        <Row>
-          24h Vol. <ItemValue>{showLoader ? <Loader /> : '12'}</ItemValue>
-        </Row>
-      </PoolInfo>
-      <PoolInfo>
-        <Row>
-          APY:<ItemValue>{showLoader ? <Loader /> : '12'}</ItemValue>
-        </Row>
-        <Row>
-          Reward APR:<ItemValue>{showLoader ? <Loader /> : '12'}</ItemValue>
-        </Row>
+        <InfoTitle>APY</InfoTitle>
+        <ItemValue>{showLoader ? <Loader /> : '13.2%'}</ItemValue>
       </PoolInfo>
       <PoolInfo>
         <DepositButton active>Deposit</DepositButton>
-        <WithdrawButton active>Withdraw</WithdrawButton>
+        <WithdrawButton active disabled>
+          Withdraw
+        </WithdrawButton>
       </PoolInfo>
     </Wrapper>
-    // </Container>
   )
 }
 
@@ -117,7 +137,7 @@ function BalanceRow({ address, symbol }: { address: string; symbol: string }) {
   return (
     <Row>
       <ImageWithFallback src={logo} alt={`${symbol} logo`} width={40} height={40} />
-      {symbol}
+      <SymbolText>{symbol}</SymbolText>
     </Row>
   )
 }

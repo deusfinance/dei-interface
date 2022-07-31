a415e65deb0515f9e784f40e1fb1f4c617d275fc
Shadow <87962799+shadowcrypto1@users.noreply.github.com>
2022-08-01 01:52:48 +0530

    fix usdc and deus amounts for new vouchers

--- src/components/App/Dashboard/DeusStats.tsx
+++ src/components/App/Dashboard/DeusStats.tsx
@@ -153,6 +153,7 @@ const SecondaryLabel = styled.span`
 // TODO: Subgraphs should read NFT creation tx and send the respective usdc redeemed. until then, this is a ugly fix
 // a utility function to adjust usdc redeemed per dei for pre and post dynamic era based on voucher number
 export function adjustedUsdcPerDei(y: number, currentTokenId: string) {
+  if (parseInt(currentTokenId) > 4031) return 0 //deiv2 era
   if (parseInt(currentTokenId) > 453) return y //post dynamic era, so the original math applies
   if (parseInt(currentTokenId) > 131) return 0.1 // tranche 3 in pre dynamic era
   if (parseInt(currentTokenId) > 52) return 0.2 // tranche 2 in pre dynamic era
@@ -162,6 +163,7 @@ export function adjustedUsdcPerDei(y: number, currentTokenId: string) {
 
 // a utility function to adjust deus redeemed per dei for pre era based on voucher number
 export function adjustedDeusPerDei(y: number, currentTokenId: string) {
+  if (parseInt(currentTokenId) > 4031) return 1 //deiv2 era
   if (parseInt(currentTokenId) > 453) return y //post dynamic era, so the original math applies
   return 2.1 - adjustedUsdcPerDei(y, currentTokenId) // pre dynamic, adjusted their vdeus to offset usdc
 }
@@ -245,7 +247,10 @@ export default function DeusStats() {
     let deusRedeemable = 0
     allVouchers?.map((voucher: Voucher) => {
       deiBurn = deiBurn + Number(formatEther(voucher?.amount || '0'))
-      usdcRedeem = usdcRedeem + Number(formatEther(voucher?.amount || '0')) * parseFloat(voucher?.y || '0')
+      usdcRedeem =
+        usdcRedeem +
+        Number(formatEther(voucher?.amount || '0')) *
+          adjustedUsdcPerDei(parseFloat(voucher?.y || '0'), voucher.currentTokenId)
       deusRedeemable =
         deusRedeemable +
         Number(formatEther(voucher?.amount || '0')) *
